bb2130da5e3ad1dafd9b6303bab5d27ff5526e62
amitaydr <amitaydr@gmail.com>
2016-01-12 13:30:57 +0200

    first commit3

---
+++ src/protocol/EchoProtocol.java
@@ -0,0 +1,79 @@
+package protocol;
+
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+
+import tokenizer.StringMessage;
+
+/**
+ * a simple implementation of the server protocol interface
+ */
+public class EchoProtocol implements AsyncServerProtocol<StringMessage> {
+
+	private boolean _shouldClose = false;
+	private boolean _connectionTerminated = false;
+
+	/**
+	 * processes a message<BR>
+	 * this simple interface prints the message to the screen, then composes a simple
+	 * reply and sends it back to the client
+	 *
+	 * @param msg the message to process
+	 * @return the reply that should be sent to the client, or null if no reply needed
+	 */
+	@Override
+	public void processMessage(StringMessage msg, ProtocolCallback<StringMessage> callback) {        
+		if (this._connectionTerminated) {
+			return ;
+		}
+		if (this.isEnd(msg)) {
+			this._shouldClose = true;
+			try {
+				callback.sendMessage(new StringMessage("Ok, bye bye"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else{
+			try {
+				callback.sendMessage(msg);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * detetmine whether the given message is the termination message
+	 *
+	 * @param msg the message to examine
+	 * @return false - this simple protocol doesn't allow termination...
+	 */
+	@Override
+	public boolean isEnd(StringMessage msg) {
+		System.out.println("calling isEnd");
+		System.out.println(msg.toString());
+		System.out.println(msg.toString()=="bye");
+		return msg.toString()=="bye";
+	}
+
+	/**
+	 * Is the protocol in a closing state?.
+	 * When a protocol is in a closing state, it's handler should write out all pending data, 
+	 * and close the connection.
+	 * @return true if the protocol is in closing state.
+	 */
+	@Override
+	public boolean shouldClose() {
+		return this._shouldClose;
+	}
+
+	/**
+	 * Indicate to the protocol that the client disconnected.
+	 */
+	@Override
+	public void connectionTerminated() {
+		this._connectionTerminated = true;
+	}
+
+}
