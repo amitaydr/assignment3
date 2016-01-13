cb2a9d9b35f2963befeedbbd51efb7ab4961d97d
amitaydr <amitaydr@gmail.com>
2016-01-13 15:20:31 +0200

    adding TBGP tokenizer

--- src/protocol/TBGP.java
+++ src/protocol/TBGP.java
@@ -1,7 +1,5 @@
 package protocol;
 
-import java.io.IOException;
-
 import app.GameManager;
 import app.GameRoom;
 import tokenizer.TBGPCommand;
@@ -10,71 +8,89 @@ import tokenizer.TBGPMessage;
 public  class TBGP implements AsyncServerProtocol<TBGPMessage> {
 	private GameRoom gameRoom;
 	private String nickname;
-	private boolean shouldclose;
+	private boolean shouldClose;
 
 	@Override
 	public void processMessage(TBGPMessage msg, ProtocolCallback<TBGPMessage> callback) {
 		TBGPCommand command = msg.getCommand();
-		switch (command){
-			case NICK :
-				TBGPMessage result;
-				if (nickname != null){
-					boolean ans = GameManager.getInstance().acquireNickname(msg.getMessage(), (TBGPProtocolCallback) callback);
-					 result = new TBGPMessage ("NICK " +(ans? "ACCEPTED":"REJECTED"), TBGPCommand.SYSMSG);
-					if (ans) nickname = msg.getMessage();
-				}else{
-					 result = new TBGPMessage ("NICK REJECTED This player alrady has a nickname- " + nickname, TBGPCommand.SYSMSG);
-				}
-				callback.sendMessage(result);
-				break;
-			case JOIN:
-				if (gameRoom != null) { //User is already in some room
-					if (!gameRoom.quit(nickname)) { //if quit returns false it means the room is in a middle of a game
-						callback.sendMessage(new TBGPMessage("JOIN REJECTED cannot leave the room "+ gameRoom +" in the middle of a game!",TBGPCommand.SYSMSG));
-						break;
+		if (command != null){
+			switch (command){
+				case NICK :
+					TBGPMessage result;
+					if (nickname != null){
+						boolean ans = GameManager.getInstance().acquireNickname(msg.getMessage(), (TBGPProtocolCallback) callback);
+						 result = new TBGPMessage ("NICK " +(ans? "ACCEPTED":"REJECTED"), TBGPCommand.SYSMSG);
+						if (ans) nickname = msg.getMessage();
+					}else{
+						 result = new TBGPMessage ("NICK REJECTED This player alrady has a nickname- " + nickname, TBGPCommand.SYSMSG);
 					}
+					callback.sendMessage(result);
+					break;
+				case JOIN:
+					if (gameRoom != null) { //User is already in some room
+						if (!gameRoom.quit(nickname)) { //if quit returns false it means the room is in a middle of a game
+							callback.sendMessage(new TBGPMessage("JOIN REJECTED cannot leave the room "+ gameRoom +" in the middle of a game!",TBGPCommand.SYSMSG));
+							break;
+						}
+					}
+					boolean ans = GameManager.getInstance().joinGameRoom(msg.getMessage(), nickname);
+					if(ans){
+						callback.sendMessage(new TBGPMessage("JOIN ACCEPTED joined the room "+ msg.getMessage(),TBGPCommand.SYSMSG));
+						gameRoom = GameManager.getInstance().searchRoom(msg.getMessage());
+					}else{ 
+						callback.sendMessage(new TBGPMessage("JOIN REJECTED room"+ msg.getMessage() +"is in the middle of a game!",TBGPCommand.SYSMSG));
+					}
+					break;
+				case LISTGAMES:
+					GameManager.getInstance().listGames();
+					break;
+				case MSG:
+					gameRoom.broadcast(nickname + ": "+ msg.getMessage());
+					break;
+				case QUIT:
+					boolean ansQuit = gameRoom.quit(nickname);
+					if (ansQuit){
+						GameManager.getInstance().exit(nickname);
+						shouldClose = true;
+						callback.sendMessage(new TBGPMessage("QUIT ACCEPTED bye bye", TBGPCommand.SYSMSG));
+					}else{
+						callback.sendMessage(new TBGPMessage("QUIT REJECTED cannot leave before game is over!", TBGPCommand.SYSMSG));
+					}				
+					break;
+				case SELECTRESP : case TXTRESP:
+					if (gameRoom != null && gameRoom.inSession()){
+						gameRoom.getGameProtocol().processMessage(msg, (TBGPProtocolCallback) callback);
+					}else{
+						callback.sendMessage(new TBGPMessage(msg.getCommand()+" REJECTED not in gameRoom or other game not in session" ,TBGPCommand.SYSMSG));
+					}
+					
+					break;
+				case STARTGAME:
+					if (gameRoom != null && !gameRoom.inSession()){
+						boolean ansStart = gameRoom.startGame(msg.getMessage());
+						callback.sendMessage(new TBGPMessage("STARTGAME "+(ansStart? "ACCEPTED":"REJECTED") ,TBGPCommand.SYSMSG));
+					}else{
+						callback.sendMessage(new TBGPMessage("STARTGAME REJECTED not in gameRoom or other game in session" ,TBGPCommand.SYSMSG));
+					}
+					break;
+				default:
+					callback.sendMessage(new TBGPMessage( msg.getCommand() +"UNIDENTIFIED this is not a user command!" ,TBGPCommand.SYSMSG));
+					break;
 				}
-				boolean ans = GameManager.getInstance().joinGameRoom(msg.getMessage(), nickname);
-				if(ans){
-					callback.sendMessage(new TBGPMessage("JOIN ACCEPTED joined the room "+ msg.getMessage(),TBGPCommand.SYSMSG));
-					gameroom = GameManager.getInstance().getGameRoom(msg.getMessage());
-				}else{ 
-					callback.sendMessage(new TBGPMessage("JOIN REJECTED room"+ msg.getMessage() +"is in the middle of a game!",TBGPCommand.SYSMSG));
-				}
-				break;
-			case LISTGAMES:
-				GameManager.getInstance().listGames();
-				break;
-			case MSG:
-				gameRoom.broadcast(nickname + ": "+ msg.getMessage());
-				break;
-			case QUIT:
-				boolean ansQuit = gameRoom.quit(nickname);
-				
-				
-				break;
-			case SELECTRESP:
-				break;
-			case STARTGAME:
-				break;
-			case TXTRESP:
-				break;
-			default:
-				break;
-			}
+		}else{
+			callback.sendMessage(new TBGPMessage( msg.getCommand() +"UNIDENTIFIED this is an unknown command!" ,TBGPCommand.SYSMSG));
+		}
 			
 	}
 
 	@Override
 	public boolean isEnd(TBGPMessage msg) {
-		// TODO Auto-generated method stub
-		return false;
+		return msg.getCommand().equals(TBGPCommand.QUIT);
 	}
 
 	@Override
 	public boolean shouldClose() {
-		// TODO Auto-generated method stub
-		return false;
+		return shouldClose;
 	}
 
 	@Override
--- src/tokenizer/TBGPMessageTokenizer.java
+++ src/tokenizer/TBGPMessageTokenizer.java
@@ -1,33 +1,95 @@
 package tokenizer;
 
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.util.Vector;
 
 public class TBGPMessageTokenizer implements MessageTokenizer<TBGPMessage> {
+	private final String _messageSeparator = "\n";
 
-	@Override
-	public void addBytes(ByteBuffer bytes) {
-		// TODO Auto-generated method stub
-		
-	}
+	   private final StringBuffer _stringBuf = new StringBuffer();
+	   /**
+		 * the fifo queue, which holds data coming from the socket. Access to the
+		 * queue is serialized, to ensure correct processing order.
+		 */
+		private final Vector<ByteBuffer> _buffers = new Vector<ByteBuffer>();
 
-	@Override
-	public boolean hasMessage() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	   private final CharsetDecoder _decoder;
+	   private final CharsetEncoder _encoder;
 
-	@Override
-	public TBGPMessage nextMessage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	   public TBGPMessageTokenizer(Charset charset) {
 
-	@Override
-	public ByteBuffer getBytesForMessage(TBGPMessage msg) throws CharacterCodingException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	      this._decoder = charset.newDecoder();
+	      this._encoder = charset.newEncoder();
+	   }
 
+	   /**
+	    * Add some bytes to the message.  
+	    * Bytes are converted to chars, and appended to the internal StringBuffer.
+	    * Complete messages can be retrieved using the nextMessage() method.
+	    *
+	    * @param bytes an array of bytes to be appended to the message.
+	    */
+	   public synchronized void addBytes(ByteBuffer bytes) {
+		   _buffers.add(bytes);
+	      
+	   }
+
+	   /**
+	    * Is there a complete message ready?.
+	    * @return true the next call to nextMessage() will not return null, false otherwise.
+	    */
+	   public synchronized boolean hasMessage() {
+		   while(_buffers.size() > 0) {
+	           ByteBuffer bytes = _buffers.remove(0);
+	           CharBuffer chars = CharBuffer.allocate(bytes.remaining());
+	 	      this._decoder.decode(bytes, chars, false); // false: more bytes may follow. Any unused bytes are kept in the decoder.
+	 	      chars.flip();
+	 	      this._stringBuf.append(chars);
+		   }
+		   return this._stringBuf.indexOf(this._messageSeparator) > -1;
+	   }
+
+	   /**
+	    * Get the next complete message if it exists, advancing the tokenizer to the next message.
+	    * @return the next complete message, null if no complete message exist. if the command is not valid returns TBGPMessage with null command field
+	    */
+	   public synchronized TBGPMessage nextMessage() {
+	      String message = null;
+	      TBGPCommand command = null;
+	      String commandString = null;
+	      int messageEnd = this._stringBuf.indexOf(this._messageSeparator);
+	      int commandEnd = this._stringBuf.indexOf(" ");
+	      if (messageEnd > -1) {
+	    	 if (commandEnd >-1 && commandEnd < messageEnd){
+		    	 commandString = this._stringBuf.substring(0, commandEnd);
+		         message = this._stringBuf.substring(commandEnd+" ".length(), messageEnd);
+		         this._stringBuf.delete(0, messageEnd+this._messageSeparator.length());
+	    	 }else{
+	    		 commandString = this._stringBuf.substring(0, messageEnd);
+		         this._stringBuf.delete(0, messageEnd+this._messageSeparator.length());
+	    	 }
+	    	 command = TBGPCommand.valueOf(commandString);	    	 
+	      }else {
+	    	  return null;
+	      }
+	      return new TBGPMessage(message, command);
+	   }
+
+	   /**
+	    * Convert the TBGP message into bytes representation, taking care of encoding and framing.
+	    *
+	    * @return a ByteBuffer with the message content converted to bytes, after framing information has been added.
+	    */
+	   public ByteBuffer getBytesForMessage(TBGPMessage msg)  throws CharacterCodingException {
+	      StringBuilder sb = new StringBuilder(msg.getCommand() + " " + msg.getMessage());
+	      sb.append(this._messageSeparator);
+	      ByteBuffer bb = this._encoder.encode(CharBuffer.wrap(sb));
+	      return bb;
+	   }
 
 }
